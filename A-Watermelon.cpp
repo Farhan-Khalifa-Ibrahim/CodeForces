49f4f89306c285f6a4bbdabdc89edfc460b1221e
Farhan-Khalifa-Ibrahim <farhankhalifa1510@gmail.com>
2021-03-01 12:04:41 +0800

    Let's start CP again

--- A-Watermelon.cpp
+++ A-Watermelon.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int weight;
-    cin>>weight;
-    if(weight<=2){
-        cout<<"NO"<<endl;
+    cin >> weight;
+    if (weight <= 2)
+    {
+        cout << "NO" << endl;
     }
-    else if(weight%2==0){
-        cout<<"YES"<<endl;
+    else if (weight % 2 == 0)
+    {
+        cout << "YES" << endl;
     }
-    else{
-        cout<<"NO"<<endl;
+    else
+    {
+        cout << "NO" << endl;
     }
 }
